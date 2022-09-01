0ceb9a01a660870954c966496125d165c70b8c42
simpan824 <simon.dagernas@elev.ga.ntig.se>
2022-09-01 13:28:49 +0200

    respons för olika gissningar
    flyttade också på userinput för jag satte den på fel ställe :P

--- NGG/src/Game.h
+++ NGG/src/Game.h
@@ -9,13 +9,31 @@ public:
 		int tries = 0;
 		std::string userinput;
 		srand(time(NULL));
-		int randomNumber = rand() % 100 + 1;
-		std::cout << "Guess a random number between 1-100" << "\n";
-		std::getline(std::cin, userinput);
+		int randomnumber = rand() % 100 + 1;
+		std::cout << "Guess a random number between 1-100 \n";
+
 		while (tries < 10)
 		{
+			std::getline(std::cin, userinput);
+			int userguess = std::stoi(userinput);
 
-			tries++;
+			if (userguess > randomnumber)
+			{
+				std::cout << "The number is smaller. \n";
+				tries++;
+			}
+
+			if (userguess < randomnumber)
+			{
+				std::cout << "The number is bigger. \n";
+				tries++;
+			}
+
+			if (userguess == randomnumber)
+			{
+				std::cout << "You guessed the right number! Good job! \n";
+				tries = 10;
+			}
 		}
 	}
 };
