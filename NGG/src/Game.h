8c513b2f5457405a99b119c60f1ae925f1b696a8
simpan824 <simon.dagernas@elev.ga.ntig.se>
2022-09-01 12:55:46 +0200

    random nummer
    skapade en random number generator mellan 1-100

--- NGG/src/Game.h
+++ NGG/src/Game.h
@@ -7,12 +7,13 @@ public:
 	void Run()
 	{
 		int tries = 0;
-
+		srand(time(NULL));
+		int randomNumber = rand() % 100 + 1;
+		std::cout << "The number is: " << randomNumber << "\n";
 		while (tries < 10)
 		{
 			std::cout << "hi" << tries+1 << "\n";
 			tries++;
 		}
-		std::cin.get();
 	}
 };
